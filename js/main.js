36f18fb5922f193b6eef5bb221769538b84b3c8b
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-25 17:31:09 +0200

    other solution

--- js/main.js
+++ js/main.js
@@ -4,23 +4,21 @@ const ul = document.querySelector ("ul.list")
 
 //creare 100 box
 for (let i = 1; i < 101; i++ ) {
-    
+
     //verifica divisibilità
     let u = (i % 3);
     let r = (i % 5);
 
     //confronti
+    let element
     if (u == 0 && r == 0) {
-        const element = `<li class="box blue">fizzbuzz</li>`;
-        ul.innerHTML += element;
+        element = `<li class="box blue">fizzbuzz</li>`;
     } else if (r == 0) {
-        const element = `<li class="box red">buzz</li>`;
-        ul.innerHTML += element;
+        element = `<li class="box red">buzz</li>`;
     } else if (u == 0) {
-        const element = `<li class="box purple">fizz</li>`;
-        ul.innerHTML += element;
+        element = `<li class="box purple">fizz</li>`;
     } else {
-        const element = `<li class="box">${i}</li>`;
-        ul.innerHTML += element;
+        element = `<li class="box">${i}</li>`;
     }
+    ul.innerHTML += element;
 }
