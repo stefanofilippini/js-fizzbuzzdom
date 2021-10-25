31930da85f7de3f8be1839d456a00a2c75d5e07b
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-25 15:46:07 +0200

    created boxes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('JS OK');
+//costanti
+const ul = document.querySelector ("ul.list")
+
+//creare 100 box
+for (let i = 1; i < 101; i++ ) {
+    console.log(i);
+    const element = `<li class="box"></li>`;
+    ul.innerHTML += element;
+}
