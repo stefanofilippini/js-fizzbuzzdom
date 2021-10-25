b1638ef1d4f6ccf7c5c7f8a581b92ae52ae41a31
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-25 16:25:46 +0200

    execise completed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@ const ul = document.querySelector ("ul.list")
 
 //creare 100 box
 for (let i = 1; i < 101; i++ ) {
-    console.log(i);
-    const element = `<li class="box"></li>`;
-    ul.innerHTML += element;
+    let u = (i % 3);
+    let r = (i % 5);
+    if (u == 0 && r == 0) {
+        const element = `<li class="box blue">fizzbuzz</li>`;
+        ul.innerHTML += element;
+    } else if (r == 0) {
+        const element = `<li class="box red">buzz</li>`;
+        ul.innerHTML += element;
+    } else if (u == 0) {
+        const element = `<li class="box purple">fizz</li>`;
+        ul.innerHTML += element;
+    } else {
+        const element = `<li class="box">${i}</li>`;
+        ul.innerHTML += element;
+    }
+
 }
